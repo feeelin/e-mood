c181ba73336dae8782240cb1a03af577c9f176d8
Artyom Koshko <artyomkoshko@gmail.com>
2024-04-27 11:20:28 +0300

    refactor a bit

--- frontend-ng/e-mood-ng/src/app/mood-dashboard/mood-dashboard.component.ts
+++ frontend-ng/e-mood-ng/src/app/mood-dashboard/mood-dashboard.component.ts
@@ -11,23 +11,60 @@ export class MoodDashboardComponent {
   private breakpointObserver = inject(BreakpointObserver);
 
   /** Based on the screen size, switch from standard to one column per row */
-  cards = this.breakpointObserver.observe(Breakpoints.Handset).pipe(
-    map(({ matches }) => {
-      if (matches) {
+  cards = this.breakpointObserver.observe(
+    [
+      '(min-width: 1600px)',
+      Breakpoints.Large,
+      Breakpoints.Medium,
+      Breakpoints.Small,
+      Breakpoints.Handset])
+    .pipe(
+      map(result => {
+        console.log(result);
+        const breakpoints = result.breakpoints;
+
+        if (this.breakpointObserver.isMatched('(min-width: 1600px)')) {
+          return [
+            { title: 'Подписчики', cols: 2, rows: 1 },
+            { title: 'Треки', cols: 1, rows: 1 },
+            { title: 'Добавить трек', cols: 1, rows: 2 },
+          ];
+        }
+        if (breakpoints[Breakpoints.Large]) {
+          return [
+            { title: 'Подписчики', cols: 2, rows: 1 },
+            { title: 'Треки', cols: 1, rows: 1 },
+            { title: 'Добавить трек', cols: 1, rows: 2 },
+          ];
+        }
+        if (breakpoints[Breakpoints.Medium]) {
+          return [
+            { title: 'Подписчики', cols: 2, rows: 1 },
+            { title: 'Треки', cols: 1, rows: 1 },
+            { title: 'Добавить трек', cols: 1, rows: 2 },
+          ];
+        }
+        if (breakpoints[Breakpoints.Small]) {
+          return [
+            { title: 'Подписчики', cols: 2, rows: 2 },
+            { title: 'Треки', cols: 2, rows: 2 },
+            { title: 'Добавить трек', cols: 2, rows: 2 },
+          ];
+        }
+        if (breakpoints[Breakpoints.Handset]) {
+          return [
+            { title: 'Подписчики', cols: 2, rows: 2 },
+            { title: 'Треки', cols: 2, rows: 2 },
+            { title: 'Добавить трек', cols: 2, rows: 2 },
+          ];
+        }
+
         return [
-          { title: 'Card 1', cols: 1, rows: 1 },
-          { title: 'Card 2', cols: 1, rows: 1 },
-          { title: 'Card 3', cols: 1, rows: 1 },
-          { title: 'Card 4', cols: 1, rows: 1 }
+          { title: 'Подписчики', cols: 2, rows: 2 },
+          { title: 'Треки', cols: 2, rows: 2 },
+          { title: 'Добавить трек', cols: 2, rows: 2 },
         ];
       }
-
-      return [
-        { title: 'Card 1', cols: 2, rows: 1 },
-        { title: 'Card 2', cols: 1, rows: 1 },
-        { title: 'Card 3', cols: 1, rows: 2 },
-        { title: 'Card 4', cols: 1, rows: 1 }
-      ];
-    })
-  );
+      )
+    );
 }
