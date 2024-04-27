c039b10930a0b51e65c55599725f7fcffecdaa62
Artyom Koshko <artyomkoshko@gmail.com>
2024-04-28 00:15:30 +0300

    update python

--- api-python/app.py
+++ api-python/app.py
@@ -1,46 +1,37 @@
-from flask import Flask, send_file, request, jsonify
-from flask_sqlalchemy import SQLAlchemy
-from diffusers import AudioLDM2Pipeline
-from IPython.display import Audio
-import torch
+from flask import Flask, send_file
+from transformers import pipeline, AutoProcessor, AutoModel
 import scipy
-import gradio as gr
-import numpy as np
-from scipy.io.wavfile import write
-import numpy as np
-import wave
 
 app = Flask(__name__)
 
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///example.db'
-db = SQLAlchemy(app)
-
-
-@app.route('/get-track')
+@app.route('/player/get-track')
 def get_track():
-    return send_file("files/1.mp3")
+    synthesiser = pipeline("text-to-speech", "suno/bark-small")
+    lyrics = """♪ I love JavaScript so much, ♪
+                ♪ It makes me feel like a pro, ♪
+                ♪ I can create anything, ♪ 
+                ♪ With just a few lines of code, ♪ 
+                ♪ I'll never get bored, ♪
+                ♪ With so many things to do, ♪
+                ♪ I'll always be learning, ♪
+                ♪ And that's why JavaScript's true, ♪
+                ♪ JavaScript makes me feel alive, ♪
+                ♪ It gives me power over all other languages, ♪
+                ♪ It helps me conquer any problem, ♪
+                ♪ And makes me feel like a boss, ♪
+                ♪ JavaScript makes me feel like a superhero, ♪
+                ♪ It makes me feel like nothing else matters, ♪
+                ♪ Because JavaScript gives me super powers, ♪
+                ♪ To do anything that I want, ♪
+                ♪ So if you're looking for inspiration, ♪
+                ♪ Or just want some good vibes, ♪
+                ♪ Look no further than JavaScript, ♪
+                ♪ Because it's what makes me feel alive! ♪"""
+    speech = synthesiser(lyrics, forward_params={"do_sample": True})
 
-
-@app.route('/generate-track', methods=['GET'])
-def generate_track():
-    repo_id = "cvssp/audioldm2"
-    pipe = AudioLDM2Pipeline.from_pretrained(
-    repo_id,
-    torch_dtype=torch.float16,
-    ).to("cuda")
-    prompt = "Создайте спокойную инструментальную музыку в стиле эмбиент."
-
-    audio = pipe(prompt).audios[0]
-    with wave.open('output.wav', 'wb') as wav_file:
-        # Устанавливаем параметры файла
-        wav_file.setnchannels(1)
-        wav_file.setsampwidth(2)
-        wav_file.setframerate(10000)
-
-        # Записываем данные в файл
-        wav_file.writeframes(audio.tobytes())
-        return send_file('output.wav', mimetype='audio/wav')
+    scipy.io.wavfile.write("bark_out.wav", rate=speech["sampling_rate"], data=speech["audio"])
+    return send_file("bark_out.wav")
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True, host='0.0.0.0', port=80)
