5fa5754bf1d19d14060f06648b831bb21bcc4c36
Valery Filatov <valiklfiatoff@yandex.ru>
2024-04-27 16:31:28 +0300

    Player is finally working

--- frontend-react/src/App.tsx
+++ frontend-react/src/App.tsx
@@ -2,14 +2,32 @@ import './App.css'
 import Header from "./components/header/header.tsx";
 import Player from "./components/player/Player.tsx";
 import PlayerControl from "./components/playerControl/playerControl.tsx";
+import {useEffect, useRef, useState} from "react";
+// @ts-ignore
+import useSound from "use-sound";
 
 
 function App() {
+
+    const [isPlaying, setIsPlaying] = useState(false);
+    const [play, { pause, duration, sound }] = useSound('https://cdn.eureka-team.ru/music/1.mp3');
+
+    const audioRef = useRef();
+    console.log(audioRef)
+
+    useEffect(() => {
+        if(isPlaying){
+            play()
+        }else{
+            pause()
+        }
+    }, [isPlaying]);
+
   return (
     <div className={'app'}>
         <Header/>
         <div className={'contentContainer'}>
-            <PlayerControl/>
+            <PlayerControl isPlaying={isPlaying} setIsPlaying={setIsPlaying} />
             <Player/>
         </div>
     </div>
