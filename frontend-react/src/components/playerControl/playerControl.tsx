0a29e87d0c2fa94f4ca21c947703d081c47484fd
dashagenze <89853583+dashagenze@users.noreply.github.com>
2024-04-28 12:15:56 +0300

    added title

--- frontend-react/src/components/playerControl/playerControl.tsx
+++ frontend-react/src/components/playerControl/playerControl.tsx
@@ -2,6 +2,8 @@
 import React from 'react';
 import ModeSelection from "../../modules/modeSelection/ModeSelection.tsx";
 import PlayerControlButtons from "../../modules/playerControlButtons/PlayerControlButtons.tsx";
+import Typography from "@mui/material/Typography";
+import classes from './playerControl.module.css'
 
 interface Props{
     isPlaying: boolean,
@@ -11,6 +13,7 @@ interface Props{
 const PlayerControl = (props: Props) => {
     return (
         <div>
+            <Typography className={classes.h} variant={'h4'} component={'h4'}>Какой сегодня муд?</Typography>
             <PlayerControlButtons isPlaying={props.isPlaying} setIsPlaying={props.setIsPlaying} />
             <ModeSelection playlistTitle={'Rock Classic super beautiful'}/>
         </div>
